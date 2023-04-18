680c31fd65c10dd635a4a920ff10ed7db6c8b504
Reed Martin <reedhmartin@gmail.com>
2023-04-17 21:24:15 -0400

    Update auth middleware function for GraphQL API compatibility and add comments explaining changes.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,8 +7,14 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.query, headers or cookies
+    let token = req.query.token || req.headers.authorization || req.cookies.token;
+
+    // Check for the token in the cookie, and set it in the header for requests that do not have it set
+    if (req.cookies.token && !req.headers.authorization) {
+      req.headers.authorization = `Bearer ${req.cookies.token}`;
+      token = req.cookies.token;
+    }
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -23,17 +29,20 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
+    } catch (err) {
+      console.log('Invalid token:', err);
       return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
     next();
   },
+
+  // function to sign a JWT token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
+    // sign the JWT token with the payload and secret, and set expiration
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
