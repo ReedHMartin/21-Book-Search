ddb82bd2530c4f812c855d2ead6a69e73be51dcf
Reed Martin <reedhmartin@gmail.com>
2023-04-18 16:41:28 -0400

    COmpleted Query object in resol ver funtion. Addded todos for Mutation object.

--- client/server/schemas/resolvers.js
+++ client/server/schemas/resolvers.js
@@ -4,16 +4,29 @@ const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 const { userController } = require('../controllers/user-controller');
 
-// TODO: Define resolver functions
+// Define resolver functions
 const resolvers = {
-    Query: {
+  Query: {
+    me: async (parent, args, context) => {
+      // Check if the user is authenticated
+      if (context.user) {
+        // If authenticated, find the user data using the user's ID and return it
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
 
-    }
+      // If not authenticated, throw an error
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 
-    Mutation: {
-        
-    }
-
+  // Handle write operations
+  Mutation: {
+    // TODO: Implement 'addUser' resolver
+    // TODO: Implement 'login' resolver
+    // TODO: Implement 'saveBook' resolver
+    // TODO: Implement 'removeBook'
+  },
 };
 
 module.exports = resolvers;
