e9f266304ab63493ebcf942baad47a52d508ba63
Reed Martin <reedhmartin@gmail.com>
2023-04-19 17:37:08 -0400

    Create a new instance of the ApolloClient with the `uri` and `cache` options.

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,12 @@ import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
+// Create a new instance of the ApolloClient with the `uri` and `cache` options
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
