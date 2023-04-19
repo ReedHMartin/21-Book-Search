62593db57030a8995d99cec7d3eb9187f75c5c0a
Reed Martin <reedhmartin@gmail.com>
2023-04-19 17:29:29 -0400

    Wrap App component with ApolloProvider for GraphQL integration.

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
